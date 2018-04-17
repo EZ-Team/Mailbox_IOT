b65875cdb6b685d7cb8113878afefb059f546a77
Jérémy Boehm <jeremyboehm77@gmail.com>
2018-04-17 17:11:04 +0200

    Merge all features

--- Mailbox_IOT.ino
+++ Mailbox_IOT.ino
@@ -1,7 +1,10 @@
 #include "Gsender.h"
 #include "wifi.h"
 
-void setup() {
+const int LASER = D5;
+int valeur = 0;
+
+void sendMailNotification() {
   Serial.begin(115200);
   wifi_connect_loop("ESGI", "Reseau-GES");
   Gsender *sender = Gsender::get_instance();
@@ -11,7 +14,7 @@ void setup() {
     ->password("ZXptYWlsYm94MjAxOAo=")
     ->from("mailbox.iot2018@gmail.com")
     ->subject("mail test");
-    if(sender->send("aurelien.duval6@gmail.com", "Setup test")) {
+    if(sender->send("aurelien.duval6@gmail.com", "Vous avez reçu du courrier !")) {
         Serial.println("Message send.");
     } else {
         Serial.print("Error sending message: ");
@@ -19,7 +22,19 @@ void setup() {
     }
 }
 
+void setup() {
+  digitalWrite(LASER, HIGH);
+  pinMode(LASER, OUTPUT);
+  Serial.begin(9600);
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
+  valeur = analogRead(A0);
 
+  if(valeur < 400)
+    sendMailNotification();
+
+  Serial.println(valeur);
+  delay(100);
 }

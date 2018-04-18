3e81577eb3a2d5e14a777304cdac1e3882af9894
Jérémy Boehm <jeremyboehm77@gmail.com>
2018-04-18 15:26:11 +0200

    Code refactoring & optimization

--- Mailbox_IOT.ino
+++ Mailbox_IOT.ino
@@ -2,27 +2,41 @@
 #include "wifi.h"
 
 const int LASER = D5;
-int valeur = 0;
+const int letter_max_value = 430;
+const int package_max_value = 200;
 
-void sendMailNotification() {
+int valeur = 0;
+int cpt = 0;
+
+Gsender* setMailParams(String smtp_server, int port, String login, String password, String mail_sender, String subject) {
+  Gsender *sender = Gsender::get_instance();
+  sender->server(smtp_server)
+    ->port(port)
+    ->login(login)
+    ->password(password)
+    ->from(mail_sender)
+    ->subject(subject);
+
+  return sender;
+}
+
+void sendMailNotification(String recipient, String body) {
   Serial.begin(115200);
   wifi_connect_loop("ESGI", "Reseau-GES");
-  Gsender *sender = Gsender::get_instance();
-  sender->server("smtp.gmail.com")
-    ->port(465)
-    ->login("bWFpbGJveC5pb3QyMDE4QGdtYWlsLmNvbQo=")
-    ->password("ZXptYWlsYm94MjAxOAo=")
-    ->from("mailbox.iot2018@gmail.com")
-    ->subject("mail test");
-    if(sender->send("aurelien.duval6@gmail.com", "Vous avez reçu du courrier !")) {
+
+  Gsender *sender = setMailParams("smtp.gmail.com", 465, "bWFpbGJveC5pb3QyMDE4QGdtYWlsLmNvbQo=", "ZXptYWlsYm94MjAxOAo=", "mailbox.iot2018@gmail.com", "Vous avez reçu du courrier !");
+
+    if(sender->send(recipient, body)) {
         Serial.println("Message send.");
     } else {
         Serial.print("Error sending message: ");
-        Serial.println(sender->getError());
+//        Serial.println(sender->getError());
     }
 }
 
 void setup() {
+  cpt = 0;
+  
   digitalWrite(LASER, HIGH);
   pinMode(LASER, OUTPUT);
   Serial.begin(9600);
@@ -32,9 +46,17 @@ void loop() {
   // put your main code here, to run repeatedly:
   valeur = analogRead(A0);
 
-  if(valeur < 400)
-    sendMailNotification();
+ if(cpt != 1) {
+   if(valeur < letter_max_value && valeur > package_max_value) {
+     sendMailNotification("mailbox.iot2018@gmail.com", "Vous avez reçu une lettre. Pensez à aller vérifier votre boîte aux lettres ! ;)");
+      cpt++;
+   } else if (valeur < package_max_value) {
+     sendMailNotification("mailbox.iot2018@gmail.com", "Vous avez reçu un colis. Pensez à aller vérifier votre boîte aux lettres ! ;)");
+     cpt++;
+   }
+ }
 
+  Serial.print("0 600 ");
   Serial.println(valeur);
-  delay(100);
+  delay(25);
 }
